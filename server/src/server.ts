83051cee80b31021f242d2880bde611b0749ba0d
Luiz Rioja <lulyrioja@gmail.com>
2023-01-17 00:10:22 -0300

    feat: create http server, setup cors and prisma and create first route

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app.listen({ port: 3333 }, () => {
+  console.log("HTTP server is running 🚀");
+});
