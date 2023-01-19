6604535d8ead533ca35d3f137adca1422c583de5
Luiz Rioja <lulyrioja@gmail.com>
2023-01-19 19:08:29 -0300

    style: add color and gridTemplate in theme extend

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090A",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
