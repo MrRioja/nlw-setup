32c9f521855e70f0c00c396bee736a2e6ab070f0
Luiz Rioja <lulyrioja@gmail.com>
2023-01-20 17:15:25 -0300

    feat(routes): create route to return a summary of the days

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -116,4 +116,31 @@ export async function appRoutes(app: FastifyInstance) {
       });
     }
   });
+
+  app.get("/summary", async (req, res) => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            on H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
+  });
 }
