565e2363c2c4829884ab01540c0fe76e8dbdbfaa
Luiz Rioja <lulyrioja@gmail.com>
2023-01-20 17:14:16 -0300

    feat(routes): create route to switch habit status

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -69,4 +69,51 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     };
   });
+
+  app.patch("/habits/:id/toggle", async (req) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(req.params);
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 }
