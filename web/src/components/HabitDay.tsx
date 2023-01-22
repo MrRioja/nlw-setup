227ef7cebe62bdcb0bf4541adde5a2455fbadc99
Luiz Rioja <lulyrioja@gmail.com>
2023-01-22 20:41:22 -0300

    feat(habitDay): add checkbox in popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,7 @@
 import clsx from "clsx";
+import { Check } from "phosphor-react";
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 
 import { ProgressBar } from "./ProgressBar";
 
@@ -36,6 +38,20 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
           </span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
