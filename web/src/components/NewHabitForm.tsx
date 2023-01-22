d035c918ebbf42c5ba7470e5d78ad2739d70538a
Luiz Rioja <lulyrioja@gmail.com>
2023-01-21 21:46:36 -0300

    feat(NewHabitForm): create and style the component

---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,29 @@
+import { Check } from "phosphor-react";
+
+export function NewHabitForm() {
+  return (
+    <form className="w-full flex flex-col mt-6">
+      <label htmlFor="title" className="font-semibold leading-tight">
+        Qual seu comprometimento?
+      </label>
+
+      <input
+        autoFocus
+        id="title"
+        type="text"
+        placeholder="ex.: Exercícios, dormir bem, etc..."
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+      />
+
+      <label>Qual a recorrência?</label>
+
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+      >
+        <Check size={20} weight="bold" />
+        Confirmar
+      </button>
+    </form>
+  );
+}
