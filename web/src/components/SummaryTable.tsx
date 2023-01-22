e7685322f169843a49d1fb9d834cb6b13b78edbe
Luiz Rioja <lulyrioja@gmail.com>
2023-01-21 21:42:51 -0300

    feat(SummaryTable): adds habitDay props and makes them random

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -25,7 +25,13 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-6 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              amount={5}
+              key={date.toString()}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
