5724de4b38b56fa91d94ca6de551fed2a874f9e1
Luiz Rioja <lulyrioja@gmail.com>
2023-01-22 16:03:07 -0300

    feat: add onPress func in button new

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,16 +1,20 @@
 import colors from "tailwindcss/colors";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
 
       <TouchableOpacity
         activeOpacity={0.7}
+        onPress={() => navigate("new")}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
