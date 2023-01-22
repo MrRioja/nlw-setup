36aeaadf580e062385cd965bd126304e501350d7
Luiz Rioja <lulyrioja@gmail.com>
2023-01-22 15:59:49 -0300

    feat: add onPress func in HabitDay

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "../components/Header";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
@@ -10,6 +11,8 @@ const datesFromYearStart = generateDatesFromYearBegging();
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -34,7 +37,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
