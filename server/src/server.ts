448542a04189dd87fd667313f6f91af2b8d7c28b
Luiz Rioja <lulyrioja@gmail.com>
2023-01-23 17:43:19 -0300

    fix: add host in appListen

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ const app = Fastify();
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({ port: 3333 }, () => {
+app.listen({ port: 3333, host: "10.0.0.112" }, () => {
   console.log("HTTP server is running 🚀");
 });
