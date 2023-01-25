6c73b31226b0d053102a3fa31247f294abd4f365
Luiz Rioja <lulyrioja@gmail.com>
2023-01-25 13:57:30 -0300

    fix: returns an empty array if there are no complete habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -60,9 +60,10 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
 
-    const completedHabits = day?.dayHabits.map((dayHabit) => {
-      return dayHabit.habit_id;
-    });
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => {
+        return dayHabit.habit_id;
+      }) ?? [];
 
     return {
       possibleHabits,
