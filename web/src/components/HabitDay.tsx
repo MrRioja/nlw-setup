4db528bce08122593adc727c94df2693ff9db514
Luiz Rioja <lulyrioja@gmail.com>
2023-01-25 14:01:49 -0300

    style(habitDay): add transition and focus properties

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -22,7 +22,9 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 border-2 rounded-lg", {
+        className={clsx(
+          "w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background",
+          {
           "bg-zinc-900 border-zinc-800": completedPercentage === 0,
           "bg-violet-900 border-violet-700":
             completedPercentage > 0 && completedPercentage < 20,
@@ -33,7 +35,8 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
           "bg-violet-600 border-violet-500":
             completedPercentage >= 60 && completedPercentage < 80,
           "bg-violet-500 border-violet-400": completedPercentage >= 80,
-        })}
+          }
+        )}
       />
 
       <Popover.Portal>
