bb43b0f1f39d9efec5e48ca2b5d9b5992aac19dd
Luiz Rioja <lulyrioja@gmail.com>
2023-01-25 18:56:31 -0300

    style: add animation in checkbox and progressBar

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import colors from "tailwindcss/colors";
 import { Feather } from "@expo/vector-icons";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 import {
   Text,
   View,
@@ -20,9 +21,13 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       className="flex-row mb-2 items-center"
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View
+          entering={ZoomIn}
+          exiting={ZoomOut}
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  withTiming,
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
 }
 
 export function ProgressBar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress);
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
-        style={{ width: `${progress}%` }}
-        className="h-3 rounded-xl bg-violet-600"
-      ></View>
+      <Animated.View style={style} className="h-3 rounded-xl bg-violet-600" />
     </View>
   );
 }
